ac57df03b9934d1c0c0a4f63f00bb0cd40658d9d
sahil <souravjangar91@gmail.com>
2021-06-04 23:54:46 +0530

    added url suppport for catalog app

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -18,4 +18,6 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+      path('catalog/', include('catalog.urls')),
+    path('', RedirectView.as_view(url='catalog/')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) # still not know what is use of this ......
