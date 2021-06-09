1af607bd7d48a1684e6048735feea4f4ab882f50
sahil <souravjangar91@gmail.com>
2021-06-09 21:42:02 +0530

    database and superSU added

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Author, Genre, Book, BookInstance , Language
+
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(BookInstance)
+admin.site.register(Language)
