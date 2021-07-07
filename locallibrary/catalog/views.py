33364cbbe20e15ac2f9c2d3d0c16a1bbcbdb89a3
sahil <souravjangar91@gmail.com>
2021-07-07 17:11:01 +0530

    added lst view panel

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
 from django.views import generic
 
+
 def index(request):
     """View function for home page of site."""
 
@@ -10,7 +11,8 @@ def index(request):
     num_instances = BookInstance.objects.all().count()
 
     # Available books (status = 'a')
-    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_instances_available = BookInstance.objects.filter(
+        status__exact='a').count()
 
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
@@ -22,7 +24,7 @@ def index(request):
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
-        'num_geners' : num_geners,
+        'num_geners': num_geners,
     }
 
     # Render the HTML template index.html with the data in the context variable
@@ -31,11 +33,14 @@ def index(request):
 
 class BookListView(generic.ListView):
     model = Book
-    context_object_name = 'my_book_list'   # your own name for the list as a template variable
-    template_name = 'books/my_arbitrary_template_name_list.html'  # Specify your own template name/location
+    # your own name for the list as a template variable
+    context_object_name = 'my_book_list'
+    # Specify your own template name/location
+    template_name = 'books/my_arbitrary_template_name_list.html'
 
     def get_queryset(self):
-        return Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title war
+        # Get 5 books containing the title war
+        return Book.objects.filter(title__icontains='war')[:5]
 
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get the context
@@ -43,3 +48,7 @@ class BookListView(generic.ListView):
         # Create any data and add it to the context
         context['some_data'] = 'This is just some data'
         return context
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
