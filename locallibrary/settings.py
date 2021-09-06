608d69be0e7dc7ba23a57480d1b15c2da77f633f
sahil <souravjangar91@gmail.com>
2021-09-06 15:59:36 +0530

    solved error

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -138,6 +138,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
